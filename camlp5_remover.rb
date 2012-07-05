420e4fd7505c062353366e8a175efee8b0987e37
diegoberti <diegiberti@gmail.com>
2012-07-05 10:23:04 +0200

    test

---
+++ camlp5_remover.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+file = File.open("atp_interactive.ml", "rb")
+contents = file.read
+
+newcontents = contents.gsub(/<<(.*?)>>/) do |s|
+  s=s.gsub(/\\/,'\&\&')
+  s=s.gsub( '<<|', "secondary_parser \"" );
+  s=s.gsub( '<<', "default_parser \"" );
+  s=s.gsub( '>>', "\"" ) 
+end
+
+File.open("atp_interactive_not_camlp5.ml", "w+") do |f|
+  f.write(newcontents)
+end
+
